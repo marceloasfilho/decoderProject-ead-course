b09b50e503b9cf44f98ecadd5f69dcc2cfbfc4a6
Marcelo Filho <marceloandradesilvafilho@gmail.com>
2023-07-29 18:16:43 -0300

    - Criação de DTO para salvar curso - Adição de métodos ao service que serão chamados pelo controller

---
+++ EAD/course/src/main/java/com/ead/course/controller/CourseController.java
@@ -0,0 +1,73 @@
+package com.ead.course.controller;
+
+import com.ead.course.dto.CourseDTO;
+import com.ead.course.model.CourseModel;
+import com.ead.course.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/course")
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequiredArgsConstructor
+public class CourseController {
+    private final CourseService courseService;
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody @Valid CourseDTO courseDTO) {
+        var courseModel = new CourseModel();
+        BeanUtils.copyProperties(courseDTO, courseModel);
+        courseModel.setCreationDateTime(LocalDateTime.now(ZoneId.of("UTC")));
+        courseModel.setLastUpdateDateTime(LocalDateTime.now(ZoneId.of("UTC")));
+        return new ResponseEntity<>(this.courseService.save(courseModel), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{courseId}")
+    public ResponseEntity<?> delete(@PathVariable(value = "courseId") UUID courseId) {
+        Optional<CourseModel> courseModel = this.courseService.findById(courseId);
+        if (courseModel.isEmpty()) {
+            return new ResponseEntity<>("Course not found", HttpStatus.NOT_FOUND);
+        }
+        this.courseService.delete(courseModel.get());
+        return new ResponseEntity<>("Course deleted successfully", HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{courseId}")
+    public ResponseEntity<?> update(@PathVariable(value = "courseId") UUID courseId, @RequestBody @Valid CourseDTO courseDTO) {
+        Optional<CourseModel> courseModel = this.courseService.findById(courseId);
+        if (courseModel.isEmpty()) {
+            return new ResponseEntity<>("Course not found", HttpStatus.NOT_FOUND);
+        }
+
+        var course = courseModel.get();
+        course.setName(courseDTO.getName());
+        course.setDescription(courseDTO.getDescription());
+        course.setImageUrl(courseDTO.getImageUrl());
+        course.setCourseStatus(courseDTO.getCourseStatus());
+        course.setCourseLevel(courseDTO.getCourseLevel());
+        course.setLastUpdateDateTime(LocalDateTime.now(ZoneId.of("UTC")));
+
+        return new ResponseEntity<>(this.courseService.save(course), HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<CourseModel>> getAllCourses() {
+        return new ResponseEntity<>(this.courseService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/getCourse/{courseId}")
+    public ResponseEntity<?> getCourse(@PathVariable(value = "courseId") UUID courseId) {
+        Optional<CourseModel> courseModel = this.courseService.findById(courseId);
+        return courseModel.isEmpty() ? new ResponseEntity<>("Course not found", HttpStatus.NOT_FOUND) : new ResponseEntity<>(courseModel.get(), HttpStatus.OK);
+    }
+}
