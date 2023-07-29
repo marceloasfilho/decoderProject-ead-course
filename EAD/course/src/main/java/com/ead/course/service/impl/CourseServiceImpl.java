ccd6bb82fc55ef9d09ed9d217eb5f7f75537b350
Marcelo Filho <marceloandradesilvafilho@gmail.com>
2023-07-29 18:12:52 -0300

    - Criação de DTO para salvar curso - Adição de métodos ao service que serão chamados pelo controller

--- EAD/course/src/main/java/com/ead/course/service/impl/CourseServiceImpl.java
+++ EAD/course/src/main/java/com/ead/course/service/impl/CourseServiceImpl.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -35,5 +37,20 @@ public class CourseServiceImpl implements CourseService {
             this.moduleRepository.deleteAll(allModulesIntoCourse);
         }
         this.courseRepository.delete(courseModel);
+    }
+
+    @Override
+    public CourseModel save(CourseModel courseModel) {
+        return this.courseRepository.save(courseModel);
+    }
+
+    @Override
+    public Optional<CourseModel> findById(UUID courseId) {
+        return this.courseRepository.findById(courseId);
+    }
+
+    @Override
+    public List<CourseModel> findAll() {
+        return this.courseRepository.findAll();
     }
 }
